1b33826aefb1d46736cea038fbccaaf0325b0cb8
Cedric Douglas <cedricdouglas@Cedrics-MacBook-Pro.local>
2024-09-16 06:28:52 -0500

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Welcome to spongebob name generator. This generator will tell you what your character name will be if you lived in bikini bottom")
+saltyorsweet = input("Salty or Sweet?\n")
+seaanimal = input("What is your favorite sea animal?\n")
+print("If you lived in bikini bottom you would be a " + saltyorsweet +" "+ seaanimal)
